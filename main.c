c4e27793c1e10caabcc12e64aed94b7bd5a5b03e
AlexGreen9 <149941423+AlexGreen9@users.noreply.github.com>
2024-04-05 13:16:54 +0700

    6.2

--- main.c
+++ main.c
@@ -1,84 +1,67 @@
 #include <stdio.h>
-#include <string.h>
-#include "contacts.h"
-
-void printMenu() {
-    printf("\n===== Contact Manager =====\n");
-    printf("1. Add Contact\n");
-    printf("2. Delete Contact\n");
-    printf("3. Display All Contacts\n");
-    printf("4. Exit\n");
-    printf("===========================\n");
-    printf("Enter your choice: ");
-}
+#include <stdlib.h>
+#include "contact.h"
 
 int main() {
-    Contact* head = NULL;
-    int choice;
-    char firstName[50], lastName[50];
+    Contact* contacts_list = NULL;
+    char choice;
+    char lastname[50];
+    char firstname[50];
+    char job[50];
+    char phone[15];
+    char email[50];
+    char social_media[50];
 
     do {
-        printMenu();
-        scanf("%d", &choice);
-        getchar(); // Capture newline character
+        printf("\nMenu:\n");
+        printf("1. Add a contact\n");
+        printf("2. View all contacts\n");
+        printf("3. Delete a contact\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf(" %c", &choice);
 
         switch(choice) {
-            case 1: // Add Contact
-                {
-                    char jobTitle[50], company[50], phoneNumber[15], email[50], twitter[50];
-                    printf("Enter first_name: ");
-                    fgets(firstName, 50, stdin);
-                    firstName[strcspn(firstName, "\n")] = 0;  // Remove newline character
-                    printf("Enter last_name: ");
-                    fgets(lastName, 50, stdin);
-                    lastName[strcspn(lastName, "\n")] = 0;  // Remove newline character
-                    printf("Enter job: ");
-                    fgets(jobTitle, 50, stdin);
-                    jobTitle[strcspn(jobTitle, "\n")] = 0;  // Remove newline character
-                    printf("Enter company: ");
-                    fgets(company, 50, stdin);
-                    company[strcspn(company, "\n")] = 0;  // Remove newline character
-                    printf("Enter phone: ");
-                    fgets(phoneNumber, 15, stdin);
-                    phoneNumber[strcspn(phoneNumber, "\n")] = 0;  // Remove newline character
-                    printf("Enter email: ");
-                    fgets(email, 50, stdin);
-                    email[strcspn(email, "\n")] = 0;  // Remove newline character
-                    printf("Enter social media: ");
-                    fgets(twitter, 50, stdin);
-                    twitter[strcspn(twitter, "\n")] = 0;  // Remove newline character
-
-                    Contact* newContact = createContact(firstName, lastName, jobTitle, company, phoneNumber, email, twitter);
-                    insertContact(&head, newContact);
-                    printf("Contact added successfully.\n");
-                }
-                break;
-            case 2: // Delete Contact
-                printf("Enter first name of contact to delete: ");
-                fgets(firstName, 50, stdin);
-                firstName[strcspn(firstName, "\n")] = 0;  // Remove newline character
-                printf("Enter last name of contact to delete: ");
-                fgets(lastName, 50, stdin);
-                lastName[strcspn(lastName, "\n")] = 0;  // Remove newline character
-
-                if(deleteContact(&head, firstName, lastName))
-                    printf("Contact deleted successfully.\n");
-                else
-                    printf("Contact not found.\n");
+            case '1':
+                printf("Enter lastname: ");
+                scanf("%s", lastname);
+                printf("Enter firstname: ");
+                scanf("%s", firstname);
+                printf("Enter job: ");
+                scanf("%s", job);
+                printf("Enter phone: ");
+                scanf("%s", phone);
+                printf("Enter email: ");
+                scanf("%s", email);
+                printf("Enter social media: ");
+                scanf("%s", social_media);
                 
+                Contact* new_contact = create_contact(lastname, firstname, job, phone, email, social_media);
+                insert_contact(&contacts_list, new_contact);
+                printf("Contact added successfully.\n");
                 break;
-            case 3: // Display All Contacts
-                displayContacts(head);
+            case '2':
+                printf("Contacts:\n");
+                print_contacts(contacts_list);
                 break;
-            case 4: // Exit
+            case '3':
+                printf("Enter the lastname of the contact you want to delete: ");
+                scanf("%s", lastname);
+                printf("Enter the firstname of the contact you want to delete: ");
+                scanf("%s", firstname);
+
+                delete_contact(&contacts_list, lastname, firstname);
+                printf("Contact deleted successfully.\n");
+                break;
+            case '4':
                 printf("Exiting program.\n");
                 break;
             default:
                 printf("Invalid choice. Please try again.\n");
         }
-
-    } while(choice != 4);
+    } while(choice != '4');
 
     return 0;
 }
+
 
