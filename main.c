21285cf5111b95db3d4dbce08d3b2e512e88ff18
AlexGreen9 <149941423+AlexGreen9@users.noreply.github.com>
2024-04-05 13:01:52 +0700

    6.1

--- main.c
+++ main.c
@@ -1,37 +1,84 @@
 #include <stdio.h>
-#include "copy.h"
-#include "two.h"
-#include "one.h"
-#include "person.h"
+#include <string.h>
+#include "contacts.h"
+
+void printMenu() {
+    printf("\n===== Contact Manager =====\n");
+    printf("1. Add Contact\n");
+    printf("2. Delete Contact\n");
+    printf("3. Display All Contacts\n");
+    printf("4. Exit\n");
+    printf("===========================\n");
+    printf("Enter your choice: ");
+}
+
 int main() {
+    Contact* head = NULL;
     int choice;
-    while (1) {
-        printf("\nChoose an option:\n");
-        printf("1. Add contact\n");
-        printf("2. Edit contact\n");
-        printf("3. Delete contact\n");
-        printf("4. List contacts\n");
-        printf("5. Exit\n");
-        printf("Enter your choice: ");
+    char firstName[50], lastName[50];
+
+    do {
+        printMenu();
         scanf("%d", &choice);
+        getchar(); // Capture newline character
+
         switch(choice) {
-            case 1:
-                add_contact();
+            case 1: // Add Contact
+                {
+                    char jobTitle[50], company[50], phoneNumber[15], email[50], twitter[50];
+                    printf("Enter first_name: ");
+                    fgets(firstName, 50, stdin);
+                    firstName[strcspn(firstName, "\n")] = 0;  // Remove newline character
+                    printf("Enter last_name: ");
+                    fgets(lastName, 50, stdin);
+                    lastName[strcspn(lastName, "\n")] = 0;  // Remove newline character
+                    printf("Enter job: ");
+                    fgets(jobTitle, 50, stdin);
+                    jobTitle[strcspn(jobTitle, "\n")] = 0;  // Remove newline character
+                    printf("Enter company: ");
+                    fgets(company, 50, stdin);
+                    company[strcspn(company, "\n")] = 0;  // Remove newline character
+                    printf("Enter phone: ");
+                    fgets(phoneNumber, 15, stdin);
+                    phoneNumber[strcspn(phoneNumber, "\n")] = 0;  // Remove newline character
+                    printf("Enter email: ");
+                    fgets(email, 50, stdin);
+                    email[strcspn(email, "\n")] = 0;  // Remove newline character
+                    printf("Enter social media: ");
+                    fgets(twitter, 50, stdin);
+                    twitter[strcspn(twitter, "\n")] = 0;  // Remove newline character
+
+                    Contact* newContact = createContact(firstName, lastName, jobTitle, company, phoneNumber, email, twitter);
+                    insertContact(&head, newContact);
+                    printf("Contact added successfully.\n");
+                }
                 break;
-            case 2:
-                edit_contact();
+            case 2: // Delete Contact
+                printf("Enter first name of contact to delete: ");
+                fgets(firstName, 50, stdin);
+                firstName[strcspn(firstName, "\n")] = 0;  // Remove newline character
+                printf("Enter last name of contact to delete: ");
+                fgets(lastName, 50, stdin);
+                lastName[strcspn(lastName, "\n")] = 0;  // Remove newline character
+
+                if(deleteContact(&head, firstName, lastName))
+                    printf("Contact deleted successfully.\n");
+                else
+                    printf("Contact not found.\n");
+                
                 break;
-            case 3:
-                delete_contact();
+            case 3: // Display All Contacts
+                displayContacts(head);
                 break;
-            case 4:
-                list_contacts();
+            case 4: // Exit
+                printf("Exiting program.\n");
                 break;
-case 5:
-                return 0;
             default:
                 printf("Invalid choice. Please try again.\n");
         }
-    }
-  //  return 0;
+
+    } while(choice != 4);
+
+    return 0;
 }
+
